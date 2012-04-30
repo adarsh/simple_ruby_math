22498a5f16c0373613f2e55d91c6086784a87070
Adarsh Pandit <adarshpandit@gmail.com>
2012-04-30 18:03:30 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Some simple Ruby problems asked of me in an interview
+
+* Print each odd number from zero to n
+* Return the maximum value of an array without using `.max`
+* Create an n by n multiplication table
---
+++ ruby_math.rb
@@ -0,0 +1,47 @@
+#!/usr/bin/env ruby
+
+def print_odd_numbers(n)
+  print "Even numbers from zero to #{n}: "
+  (0..n).each { |number| print "#{number} " if (number % 2) == 1 }
+  puts
+end
+
+print_odd_numbers(30)
+
+
+def return_max_value_of_array(array)
+  max_value = nil
+
+  array.each do |element|
+    if max_value.nil?
+      max_value = element
+    end
+
+    if element > max_value
+      max_value = element
+    end
+  end
+
+  puts "The max value is #{max_value} for the array #{array}"
+end
+
+array = [1,4,6,8,99]
+return_max_value_of_array(array)
+
+
+def multiplication_table(n)
+  results = []
+
+  (1..n).each do |row_index|
+    row = []
+    (1..n).each { |column_index| row << row_index * column_index }
+    results << row
+  end
+
+  results.each do |row|
+    header = '%-3s ' * row.length
+    puts header % row
+  end
+end
+
+multiplication_table(5)
